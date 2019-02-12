47e795203d1605a02d132754aba4c0a77b5aeec6
Jonathan Grizou <jonathan.grizou@gmail.com>
2019-02-12 16:39:32 +0100

    npm+parcel

---
+++ npm_parcel/src/index.js
@@ -0,0 +1,5 @@
+import main from './main';
+
+console.log('hello world');
+
+main();
---
+++ npm_parcel/src/main.js
@@ -0,0 +1,7 @@
+// import d'un module CSS
+import classes from './main.css';
+
+export default () => {
+  console.log(classes);
+  console.log('In main');
+};
