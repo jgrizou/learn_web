e628d61a3ea6eef972e721e6d1aecddd8719a84c
Jonathan Grizou <jonathan.grizou@gmail.com>
2019-02-12 17:35:56 +0100

    vue_parcel setting

---
+++ parcel_vue_default/src/App.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div id="app">
+    <h1>Hello Parcel from Vue 📦 🚀 {{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'custom message'
+    };
+  },
+}
+</script>
+
+<style lang="css">
+html,
+body {
+  height: 100%;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol';
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+h1 {
+  font-weight: 300;
+}
+</style>
